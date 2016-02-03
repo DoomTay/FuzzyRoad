2e1b4b7f817804b18323f11734a13b342e2d2807
DoomTay <bluejedi@cfl.rr.com>
2016-02-02 20:54:14 -0500

    Driveable car

---
+++ FuzzyRoad_unity/Assets/Car.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public Rigidbody[] wheels;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Mathf.Approximately(Input.GetAxis ("Acceleration"),0))
+		{
+			foreach(Rigidbody wheel in wheels)
+			{
+				wheel.AddTorque(transform.right * 1000 * Input.GetAxis ("Acceleration"));
+			}
+		}
+	}
+}
