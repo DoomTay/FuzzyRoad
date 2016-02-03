d7ec63aef6ad68cb5939aaff5d7cad274b8051d5
DoomTay <bluejedi@cfl.rr.com>
2016-02-03 10:13:37 -0500

    Experiment with wheel colliders

--- FuzzyRoad_unity/Assets/Car.cs
+++ FuzzyRoad_unity/Assets/Car.cs
@@ -6,19 +6,23 @@ public class Car : MonoBehaviour {
 	public Rigidbody[] wheels;
 	public Transform[] frontWheels;
 
+	public float speed;
+
 	// Use this for initialization
 	void Start () {
-	
+		foreach(Rigidbody wheel in wheels)
+		{
+			wheel.maxAngularVelocity = Mathf.Infinity;
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!Mathf.Approximately(Input.GetAxis ("Acceleration"),0))
+		print (1000 * speed * Input.GetAxis ("Acceleration"));
+		foreach(Rigidbody wheel in wheels)
 		{
-			foreach(Rigidbody wheel in wheels)
-			{
-				wheel.AddTorque(transform.right * 1000 * Input.GetAxis ("Acceleration"));
-			}
+			wheel.AddTorque(transform.right * 1000 * speed * Input.GetAxis ("Acceleration"));
 		}
 
 		foreach(Transform frontWheel in frontWheels)
---
+++ FuzzyRoad_unity/Assets/CarColliders.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarColliders : MonoBehaviour {
+
+	public WheelCollider[] wheels;
+	public WheelCollider[] frontWheels;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (transform.InverseTransformDirection (GetComponent<Rigidbody> ().velocity).z);
+		foreach(WheelCollider wheel in wheels)
+		{
+			wheel.motorTorque = speed * 100 * Input.GetAxis("Acceleration");
+			if(oppositeSides(transform.InverseTransformDirection (GetComponent<Rigidbody> ().velocity).z, Input.GetAxis("Acceleration")))
+			{
+				print ("Braking");
+				wheel.brakeTorque = 10000 * speed;
+			}
+			else wheel.brakeTorque = 0;
+		}
+
+		foreach(WheelCollider frontWheel in frontWheels)
+		{
+			frontWheel.steerAngle = 40 * Input.GetAxis("Steering");
+		}
+	}
+
+	bool oppositeSides(float a, float b)
+	{
+		return (a > 0 && b < 0) || (a < 0 && b > 0);
+	}
+}
