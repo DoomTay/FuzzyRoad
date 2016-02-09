e1affce9d4cb13cd8fa4ab879d07e3b758d99ed2
davecurtisc <davidcurtis@knights.ucf.edu>
2016-02-09 05:57:52 -0500

    Models, Menu/Pause system, Environment
    - Models imported
    - Folder system managed
    - Main menu system
    - Pause menu system
    - Initial world design

---
+++ FuzzyRoad_unity/Assets/Scripts/pause_menu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;// we need this namespace in order to access UI elements within our script
+using System.Collections;
+
+public class pause_menu : MonoBehaviour {
+
+	public Canvas pauseMenu;
+	public Button continueText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		
+		pauseMenu.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+				
+				Time.timeScale = 0;
+				pauseMenu.enabled = true;
+			}
+		}
+
+	
+	public void UnPause(){
+
+		Time.timeScale = 1;
+		pauseMenu.enabled = false;
+
+		}
+
+	public void Exit_game(){
+	
+		Application.Quit();
+	}
+}
+
+
