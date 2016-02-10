31199c567d738e0b96562950b4387114618a7e50
davecurtisc <davidcurtis@knights.ucf.edu>
2016-02-09 19:08:13 -0500

    Timer, Environment
    - Created a timer that ends the game after so many minutes
    - Some environment art

---
+++ FuzzyRoad_unity/Assets/Scripts/ui_timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ui_timer : MonoBehaviour {
+	public Text timerLabel;
+
+	private float time;
+
+	private float minutes;
+	private float seconds;
+	private float fraction;
+
+
+	void Update() {
+		time += Time.deltaTime;
+
+		minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+		seconds = time % 60;//Use the euclidean division for the seconds.
+		fraction = (time * 100) % 100;
+
+		//update the label value
+		timerLabel.text = string.Format ("{0:00} : {1:00}", minutes, seconds);
+
+		if (minutes >= 1) {
+		
+			Application.LoadLevel (0);
+			Debug.Log ("Timer has ended");
+		}
+	}
+}
