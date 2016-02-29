8ecbb92b880371ab850dccacd46394fab8a0c27b
davecurtisc <davidcurtis@knights.ucf.edu>
2016-02-29 11:10:24 -0500

    Level design upgrade, character select screen
    - level design still needs work.
    - character select screen is not finished

---
+++ FuzzyRoad_unity/Assets/Scripts/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed = .01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.down * 1.5f);
+
+		if (Input.GetButtonDown ("Fire1")) {
+		
+			transform.Rotate(0, speed*Time.deltaTime,0);
+		}
+	
+	}
+}
