181817bf8c9df41f368ea6eb4251a6390147671f
DoomTay <bluejedi@cfl.rr.com>
2016-03-01 20:36:18 -0500

    Finished Camera leveling

--- FuzzyRoad_unity/Assets/LevelCamera.cs
+++ FuzzyRoad_unity/Assets/LevelCamera.cs
@@ -11,9 +11,7 @@ public class LevelCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.RotateAround(transform.root.position, transform.right, -transform.eulerAngles.x);
-
-		//transform.RotateAround(transform.root.position, transform.up, transform.eulerAngles.y - (transform.root.eulerAngles.y - transform.eulerAngles.y) * .10f);
-		transform.RotateAround(transform.root.position, transform.up, transform.root.eulerAngles.y - transform.eulerAngles.y);
+		transform.RotateAround(transform.root.position, transform.up, (transform.root.eulerAngles.y - transform.eulerAngles.y) * 0.1f);
 		transform.RotateAround(transform.root.position, transform.forward, -transform.eulerAngles.z);
 	}
 }
